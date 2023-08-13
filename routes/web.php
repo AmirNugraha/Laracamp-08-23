23425c4e0f04b1c02c668a7dba66be98709e15a7
Amir Nugraha <muhammadamirngrh@gmail.com>
2023-08-13 21:40:48 +0800

    integration dashboard user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,18 +21,19 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/login,', function () {
-    return view('login');
-})->name('login');
 
-Route::get('/checkout,', function () {
-    return view('checkout');
-})->name('checkout');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/success-checkout,', function () {
-    return view('success-checkout');
-})->name('success-checkout');
+    // checkout routes
 
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camps:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camps}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    // User dashboard routes
+
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
 
 // Socialite Routes
 
@@ -39,14 +41,9 @@ Route::get('sign-in-google', [UserController::class, 'google'])->name('user.logi
 Route::get('/auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__ . '/auth.php';
